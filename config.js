55a9563ba2ac9afd2aa3cba22dadd3d881ba0a8f
dpradyu-ui <dpradyu@gmail.com>
2026-02-02 00:00:42 +0530

    Update config.js 2

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come to Mumbai ASAP and let me take you on romantic date in exchange for a big warm hug and a huge kiss!",
+        message: "Now come to Mumbai ASAP and let me take you on romantic date in exchange for a big warm hug and a huge kiss!!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
